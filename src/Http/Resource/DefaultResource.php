16d4545faef13f376ede4e05983b56e8902c9c93
Luiz Fernando Bernardes de Paula <nandovga@hotmail.com>
2024-08-28 12:02:41 -0300

    [2.x] - Add resource DefaultResource

---
+++ src/Http/Resource/DefaultResource.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Orangesix\Http\Resource;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class DefaultResource extends JsonResource
+{
+    /**
+     * Transforme os dados para ser utilizados
+     * @return array<int|string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        $data = [];
+        if (method_exists($this->resource, 'getAttributes')) {
+            $data = $this->resource->getAttributes();
+        }
+        return array_merge($data);
+    }
+}
